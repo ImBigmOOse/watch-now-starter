c9bc485023c4de787c4abac5344835ffb41c81cf
ImBigmOOse <75133820+ImBigmOOse@users.noreply.github.com>
2020-12-05 09:48:22 -0500

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
   "id": "com.syncler.watch.now",
   "version": 1,
   "classPath": "watchNow.WatchNowPackage",
-  "permaUrl": "https://raw.githubusercontent.com/synclerd/watch-now-starter/main/package.js"
+  "permaUrl": "https://github.com/ImBigmOOse/watch-now-starter/blob/main/package.js"
 }
 */
 
